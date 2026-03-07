edb26f08956e773ad471949651e13733448dc48c
Felix <twchenfeng@gmail.com>
2026-03-07 12:58:46 +0800

    refactor

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
         <footer>
             <p>圖片版權為繪師所有，僅用於網站呈現</p>
             <p>
-                Copyright© 2025 Development: <span data-text="Felix"><a href="https://github.com/felixchen-tw" target="_blank">Felix</a></span>
+                Copyright© 2026 Development: <span data-text="Felix"><a href="https://github.com/felixchen-tw" target="_blank">Felix</a></span>
             </p>
         </footer>
     );
--- src/Pages/Fan/Fan.tsx
+++ src/Pages/Fan/Fan.tsx
@@ -5,21 +5,20 @@ import fan from '../../assets/Fan.jpeg'
 function Fan() {
   return (
     <div className='fan-container'>
-        <div className="right-title-bg">
-
-        </div>
-    <div className="container">
-      <h2 className="right-title">粉絲形象</h2>
-    </div>
-    <div className="fan-content-bg">
-      <div className="container">
+      <div className="fan-content-bg">
+        <div className="container">
         <div className="fan-content">
-          <div className="fan-subtitle">
-            <h3 className="direct">設計: <a className="fan-designer" href="https://x.com/MED0medo_3JYO" target="_blank">MEDO</a></h3>
+          <div className="container-header">
+            <h2>粉絲形象</h2>
           </div>
-          <a className="fan-img" href="https://x.com/MED0medo_3JYO/status/1795671550571823485/photo/1" target="_blank">
-            <img src={fan} alt="魚玥飯" />
-          </a>
+          <div className="fan-inner-content">
+            <div className="fan-subtitle">
+              <h3 className="direct">設計: <a className="fan-designer" href="https://x.com/MED0medo_3JYO" target="_blank">MEDO</a></h3>
+            </div>
+            <a className="fan-img" href="https://x.com/MED0medo_3JYO/status/1795671550571823485/photo/1" target="_blank">
+              <img src={fan} alt="魚玥飯" />
+            </a>
+          </div>
         </div>
       </div>
     </div>
