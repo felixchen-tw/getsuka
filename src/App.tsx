cf0adffe8fac7aad36c466932a36f0cbd92a2532
Felix <twchenfeng@gmail.com>
2026-03-20 14:36:00 +0800

    modify other-info

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
 import './App.css'
 
-import Navbar from './Components/Navbar';
+// import Navbar from './Components/Navbar';
 import Title from './Components/Title';
 import Vtuber from './Pages/Vtuber';
 import Fan from './Pages/Fan';
 import Character from './Pages/Character';
-import YoutubeInfo from './Components/YoutubeInfo';
+// import YoutubeInfo from './Components/YoutubeInfo';
 import Footer from './Components/Footer'
 
-import GetsukaEat  from './assets/getsuka-eat.png'
-import getsukaStar from './assets/getsuka-star.png'
-import getsukaHappy from './assets/getsuka-happy.png'
 
 function App() {
   return(
     <div>
-      <div className='navbar-container'>
-        <Navbar />
-      </div>
+
       <section id ="intro">
         <div className='intro-container'>
-          <div>
-            <Title text='介紹' />
-          </div>
           <div className="vtuber-fan-wrapper">
             <div className="vtuber-wrapper">
               <Vtuber />
             </div>
           </div>
-          <YoutubeInfo />
+          {/* <YoutubeInfo /> */}
           <div id="fan" className="fan-wrapper">
             <Fan />
           </div>
@@ -44,22 +36,26 @@ function App() {
           </div>
         </div>
       </section>
-      <section id="more-info">
+      <section id="other-info">
         <div>
-          <Title text='更多資訊' />
+          <Title text='其他' />
         </div>
-        <div className='more-info-container'>
-          <a href='https://docs.google.com/spreadsheets/d/1sOmeablnN8j1IdqmpXhtWDvST33AsAzPbziVyU6fGj4/edit?usp=drivesdk' target='_blank'>
-            <p className="card-header">玥華小檔案</p>
-            <img src={getsukaHappy} alt="更多資訊" />
+        <div className='other-info-container'>
+          <a href='https://p.ecpay.com.tw/5AB9B96' target='_blank' rel="noopener noreferrer">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"/></svg>
+            <span>綠界</span>
           </a>
-          <a href='https://p.ecpay.com.tw/5AB9B96' target='_blank'>
-            <p className="card-header">綠界</p>
-            <img src={GetsukaEat} alt='綠界' />
+          <a href='https://sound.getsuka.net' target='_blank' rel="noopener noreferrer">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"/><path d="M15.54 8.46a5 5 0 0 1 0 7.07"/><path d="M19.07 4.93a10 10 0 0 1 0 14.14"/></svg>
+            <span>音效按鈕</span>
           </a>
-          <a href='https://gestuka.fandom.com/' target='_blank'>
-            <p className="card-header">Wiki</p>
-            <img src={getsukaStar} alt="Wiki" />
+          <a href='https://docs.google.com/spreadsheets/d/1sOmeablnN8j1IdqmpXhtWDvST33AsAzPbziVyU6fGj4/edit?usp=drivesdk' target='_blank' rel="noopener noreferrer">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"/><path d="M14 2v4a2 2 0 0 0 2 2h4"/><path d="M10 9H8"/><path d="M16 13H8"/><path d="M16 17H8"/></svg>
+            <span>玥華小檔案</span>
+          </a>
+          <a href='https://gestuka.fandom.com/' target='_blank' rel="noopener noreferrer">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"/><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"/></svg>
+            <span>Wiki</span>
           </a>
         </div>
       </section>
